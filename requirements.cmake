5c2b1ecf6b7ddf7b6bf35cd0bfe42cab60e025a1
Kirit Sælensminde <kirit@kirit.com>
2021-06-21 08:57:22 +0700

    Include exceptions library.

--- requirements.cmake
+++ requirements.cmake
@@ -10,3 +10,14 @@ if(NOT felspar-test_POPULATED)
     FetchContent_Populate(felspar-test)
     add_subdirectory(${felspar-test_SOURCE_DIR} ${felspar-test_BINARY_DIR})
 endif()
+
+FetchContent_Declare(
+        felspar-exceptions
+        GIT_REPOSITORY https://github.com/Felspar/exceptions.git
+        GIT_TAG main
+    )
+FetchContent_GetProperties(felspar-exceptions)
+if(NOT felspar-exceptions_POPULATED)
+    FetchContent_Populate(felspar-exceptions)
+    add_subdirectory(${felspar-exceptions_SOURCE_DIR} ${felspar-exceptions_BINARY_DIR})
+endif()
