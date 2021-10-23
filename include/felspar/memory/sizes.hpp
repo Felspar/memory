f6731a1ff0c3b9ba08544a37121fb4968999bc29
Kirit Sælensminde <kirit@kirit.com>
2021-10-23 11:09:21 +0700

    Add way to get an aligned offset

--- include/felspar/memory/sizes.hpp
+++ include/felspar/memory/sizes.hpp
@@ -9,9 +9,17 @@ namespace felspar::memory {
 
     /// Calculate how big a space a type will need in a contiguous memory area
     /// taking into account its size and alignment
-    constexpr inline std::size_t block_size(
+    constexpr std::size_t block_size(
             std::size_t const size, std::size_t const align) noexcept {
         return (align * ((size + align - 1) / align));
+    }
+
+
+    /// Given a base position, calculate the next one above it at the requested
+    /// alignment
+    constexpr std::size_t aligned_offset(
+            std::size_t const base, std::size_t const alignment) {
+        return (base + alignment - 1) & ~(alignment - 1);
     }
 
 
