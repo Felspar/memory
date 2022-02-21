b221aa6476919de0cff28185847be98004912bd0
Kirit Sælensminde <kirit@kirit.com>
2022-02-21 12:17:35 +0700

    Add some tests for the fixed pool allocator

---
+++ test/run/fixed-pool.pmr.cpp
@@ -0,0 +1,43 @@
+#include <felspar/memory/fixed-pool.pmr.hpp>
+#include <felspar/test.hpp>
+
+
+namespace {
+
+
+    auto const suite = felspar::testsuite("fixed-pool.pmr");
+
+
+    auto const s = suite.test("small", [](auto check) {
+        felspar::memory::fixed_pool fp{
+                512, felspar::pmr::new_delete_resource()};
+
+        void *a1 = fp.allocate(100);
+        void *a2 = fp.allocate(100);
+        check(a1) != a2;
+        fp.deallocate(a1, 100);
+        fp.deallocate(a2, 100);
+
+        void *a3 = fp.allocate(200);
+        check(a3) == a2;
+        fp.deallocate(a3, 200);
+    });
+
+
+    auto const b = suite.test("big", [](auto check) {
+        felspar::memory::fixed_pool fp{
+                512, felspar::pmr::new_delete_resource()};
+
+        void *a1 = fp.allocate(1000);
+        void *a2 = fp.allocate(1000);
+        check(a1) != a2;
+        fp.deallocate(a1, 1000);
+        fp.deallocate(a2, 1000);
+
+        void *a3 = fp.allocate(2000);
+        check(a3) != a2;
+        fp.deallocate(a3, 2000);
+    });
+
+
+}
