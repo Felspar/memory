44581a7fd22b2c00557b6b0ce4f04541139e16d3
Kirit Sælensminde <kirit@felspar.com>
2023-02-12 14:01:37 +0700

    Clean up various comments

--- include/felspar/memory/control.hpp
+++ include/felspar/memory/control.hpp
@@ -19,17 +19,15 @@ namespace felspar::memory {
 
 
     /// ## `control`
-    /**
-        Control block for owned memory.
-     */
+    /// Control block for shared memory
     struct control {
         /// Use a virtual destructor for type erasure
         virtual ~control() = default;
 
         /**
-           Creates a new control block with an ownership count of 1 that wraps
-           the specified object and returns a pointer to both the control block
-           and the object after it has been moved into its new location.
+         * Creates a new control block with an ownership count of 1 that wraps
+         * the specified object and returns a pointer to both the control block
+         * and the object after it has been moved into its new location.
          */
         template<typename S>
         static std::pair<std::unique_ptr<control>, S *> wrap_existing(S &&s) {
@@ -44,8 +42,8 @@ namespace felspar::memory {
             return {std::move(made), pitem};
         }
         /**
-         Allocate a chunk of memory. Return the control block together with a
-         span that encompasses the memory allocated.
+         * Allocate a chunk of memory. Return the control block together with a
+         * span that encompasses the memory allocated.
          */
         static auto
                 allocate(std::size_t const bytes, std::size_t const alignment) {
@@ -67,11 +65,11 @@ namespace felspar::memory {
         }
 
         /**
-           Increment and decrement the usage count. We never need to do
-           anything after the increment so we don't need to know the exact
-           count. Decrementing is another matter though as we have to get the
-           exact right count for zero as we will destruct the control block at
-           that point (which in turn will destruct the owned memory.
+         * Increment and decrement the usage count. We never need to do anything
+         * after the increment so we don't need to know the exact count.
+         * Decrementing is another matter though as we have to get the exact
+         * right count for zero as we will destruct the control block at that
+         * point (which in turn will destruct the owned memory.
          */
         static control *increment(control *c) noexcept {
             if (c) { ++c->ownership_count; }
@@ -90,8 +88,8 @@ namespace felspar::memory {
 
     /// ## `owner_tracking_iterator`
     /**
-        This type can be used to wrap an iterator so that it will also carry
-        a pointer to a control block.
+     * This type can be used to wrap an iterator so that it will also carry a
+     * pointer to a control block.
      */
     template<typename Iter, typename Control>
     struct owner_tracking_iterator final {
--- include/felspar/memory/sizes.hpp
+++ include/felspar/memory/sizes.hpp
@@ -7,8 +7,14 @@
 namespace felspar::memory {
 
 
-    /// Calculate how big a space a type will need in a contiguous memory area
-    /// taking into account its size and alignment
+    /// ## Memory block size
+    /**
+     * Calculate how big a space a type will need in a contiguous memory area.
+     *
+     * For a given sized memory allocation and an alignment requirement for the
+     * following memory, this returns the total size of memory that needs to be
+     * allocated to maintain the following alignment.
+     */
     constexpr std::size_t block_size(
             std::size_t const size, std::size_t const align) noexcept {
         return (align * ((size + align - 1) / align));
