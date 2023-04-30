aea94061a48f9102af41efce06da60dbead7f223
Kirit Sælensminde <kirit@felspar.com>
2023-04-30 11:06:08 +0700

    Update a number of comments

--- include/felspar/memory/accumulation_buffer.hpp
+++ include/felspar/memory/accumulation_buffer.hpp
@@ -7,8 +7,11 @@
 namespace felspar::memory {
 
 
-    /// A buffer into which data can be accumulated at the end, and consumed
-    /// from the front
+    /// ## Accumulation buffer
+    /**
+     * A buffer into which data can be accumulated at the end, and consumed from
+     * the front.
+     */
     template<typename T>
     class accumulation_buffer final {
         using buffer_type = shared_buffer<T>;
@@ -22,20 +25,20 @@ namespace felspar::memory {
       public:
         using value_type = T;
 
-        /// Constructors
+        /// ### Constructors
         explicit accumulation_buffer(std::size_t mb) : min_buffer{mb} {}
         accumulation_buffer(accumulation_buffer const &) = default;
         accumulation_buffer(accumulation_buffer &&) = default;
 
-        /// Assignment
+        /// ### Assignment
         accumulation_buffer &operator=(accumulation_buffer const &) = default;
         accumulation_buffer &operator=(accumulation_buffer &&) = default;
 
-        /// Information about the current state of the buffer
+        /// ### Information about the current state of the buffer
         bool empty() const noexcept { return occupied.empty(); }
         auto size() const noexcept { return occupied.size(); }
 
-        /// Grow the buffer
+        /// ### Grow the buffer
         template<typename V = value_type>
         void ensure_length(std::size_t const count, V &&t = {}) {
             if (occupied.size() < count) {
@@ -59,7 +62,7 @@ namespace felspar::memory {
             }
         }
 
-        /// Access to the buffer
+        /// ### Access to the buffer
         value_type &operator[](std::size_t const i) { return occupied[i]; }
         value_type const &operator[](std::size_t const i) const {
             return occupied[i];
@@ -67,7 +70,7 @@ namespace felspar::memory {
         auto begin() { return occupied.begin(); }
         auto end() { return occupied.end(); }
 
-        /// Split the buffer
+        /// ### Split the buffer
         auto first(std::size_t const count) {
             auto const part = occupied.first(count);
             occupied = occupied.subspan(count);
--- include/felspar/memory/shared_buffer.hpp
+++ include/felspar/memory/shared_buffer.hpp
@@ -15,7 +15,7 @@ namespace felspar::memory {
     class shared_buffer_view;
 
 
-    /// A shared memory buffer
+    /// ## A shared memory buffer
     template<typename T>
     class shared_buffer final {
         friend class accumulation_buffer<T>;
@@ -25,9 +25,9 @@ namespace felspar::memory {
         using control_type = control;
         using buffer_type = std::span<T>;
 
-        /// Constructor used to wrap the vector allocation
+        /// ### Private constructors
         explicit shared_buffer(
-                std::pair<std::unique_ptr<control>, vector_type *> alloc)
+                std::pair<std::unique_ptr<control_type>, vector_type *> alloc)
         : buffer{alloc.second->data(), alloc.second->size()},
           owner{alloc.first.release()} {}
         shared_buffer(control_type *o, buffer_type b)
@@ -36,7 +36,7 @@ namespace felspar::memory {
       public:
         using value_type = T;
 
-        /// Construction, destruction and assignment
+        /// ### Construction, destruction and assignment
         shared_buffer() {}
         shared_buffer(shared_buffer const &sb)
         : buffer{sb.buffer}, owner{control_type::increment(sb.owner)} {}
@@ -57,20 +57,20 @@ namespace felspar::memory {
         }
         ~shared_buffer() { control_type::decrement(owner); }
 
-        /// Allocation
+        /// ### Allocation
         template<typename V = value_type>
         static shared_buffer allocate(std::size_t const count, V &&v = {}) {
             return shared_buffer{control_type::wrap_existing(
                     vector_type(count, std::forward<V>(v)))};
         }
 
-        /// Information about the buffer
+        /// ### Information about the buffer
         bool empty() const noexcept { return buffer.empty(); }
         auto size() const noexcept { return buffer.size(); }
         /// Return the memory control block
         control_type *control_block() const noexcept { return owner; }
 
-        /// Access to the buffer
+        /// ### Access to the buffer
         value_type &operator[](std::size_t const i) { return buffer[i]; }
         value_type const &operator[](std::size_t const i) const {
             return buffer[i];
@@ -80,7 +80,7 @@ namespace felspar::memory {
 
         value_type const *data() const noexcept { return buffer.data(); }
 
-        /// Implicit conversions
+        /// ### Implicit conversions
         operator std::span<value_type>() { return buffer; }
 
       private:
@@ -89,7 +89,7 @@ namespace felspar::memory {
     };
 
 
-    /// A view onto a shared memory buffer
+    /// ## A view onto a shared memory buffer
     template<typename T>
     class shared_buffer_view {
       public:
@@ -100,10 +100,10 @@ namespace felspar::memory {
         shared_buffer_view(shared_buffer<std::remove_cv_t<value_type>> const &sb)
         : buffer{sb.buffer}, owner{sb.owner} {}
 
-        /// Information about the buffer
+        /// ### Information about the buffer
         bool empty() const noexcept { return buffer.empty(); }
         auto size() const noexcept { return buffer.size(); }
-        /// Return the memory control block
+        /// ### Return the memory control block
         control_type *control_block() const noexcept { return owner; }
 
       private:
