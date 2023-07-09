e24384ced10baa89dc5ec276717c61ca7eb36fbb
Kirit Sælensminde <kirit@felspar.com>
2023-07-09 18:27:17 +0700

    Various tweaks to the APIs and implementation

--- include/felspar/memory/any_buffer.hpp
+++ include/felspar/memory/any_buffer.hpp
@@ -1,43 +1,70 @@
 #pragma once
 
 
-#include <felspar/memory/holding_pen.hpp>
+#include <felspar/exceptions.hpp>
 
+#include <array>
 #include <typeinfo>
 
 
 namespace felspar::memory {
 
 
-    /// A buffer that can be used to transport any type and from which a value
-    /// of the correct type can be later retrieved
+    /// ## A buffer for any object
+    /**
+     * A buffer that can be used to transport any type and from which a value of
+     * the correct type can be later retrieved.
+     *
+     * The buffer contains a small amount of memory meaning that the object can
+     * be embedded.
+     */
     struct any_buffer {
         static std::size_t constexpr alignment = 16;
-        static std::size_t constexpr buffer_size = 64;
+        static std::size_t constexpr buffer_size = 128;
 
+
+        /// ### Construction
         constexpr any_buffer() noexcept {}
         any_buffer(any_buffer &&o)
         : type{o.type}, move_into{o.move_into}, deleter{o.deleter} {
             if (move_into) { move_into(buffer.data(), o.buffer.data()); }
         }
         any_buffer(any_buffer const &) = delete;
-        ~any_buffer() {
-            if (deleter) { deleter(buffer.data()); }
+        ~any_buffer() { destroy(); }
+
+        /// #### Place the object into the buffer
+        template<typename T>
+        any_buffer(T object) {
+            emplace<T>(std::move(object));
         }
 
-        any_buffer &operator=(any_buffer &&) = delete;
+        any_buffer &operator=(any_buffer &&o) {
+            destroy();
+            type = std::exchange(o.type, nullptr);
+            move_into = std::exchange(o.move_into, nullptr);
+            deleter = std::exchange(o.deleter, nullptr);
+            if (o.move_into) { move_into(buffer.data(), o.buffer.data()); }
+            return *this;
+        }
+        template<typename T>
+        any_buffer &operator=(T t) {
+            destroy();
+            emplace<T>(std::move(t));
+            return *this;
+        }
+
         any_buffer &operator=(any_buffer const &) = delete;
 
-        /// Place the object into the buffer
-        template<typename T>
-        any_buffer(T object) : type(&typeid(T)) {
+        template<typename T, typename... Args>
+        void emplace(Args... args) {
             static_assert(
                     std::alignment_of_v<T> <= alignment,
                     "Alignment requirement of held type is too high");
             static_assert(
                     sizeof(T) <= buffer_size,
                     "Large object support not yet implemented");
-            new (buffer.data()) T{std::move(object)};
+            type = &typeid(T);
+            new (buffer.data()) T{std::forward<Args>(args)...};
             move_into = [](std::byte *into, std::byte *from) {
                 new (into) T{std::move(*reinterpret_cast<T *>(from))};
             };
@@ -46,38 +73,64 @@ namespace felspar::memory {
             };
         }
 
-        /// Return true if there is a held object
+        /// ### Queries
+
+        /// #### Return true if there is a held object
         explicit operator bool() const noexcept {
             return type and move_into and deleter;
         }
 
-        /// Retrieve the value out of the buffer
-        template<typename T>
-        holding_pen<T> fetch() {
-            if (not type) {
-                return {};
-            } else if (typeid(T) == *type) {
-                return {std::move(
-                        *std::launder(reinterpret_cast<T *>(buffer.data())))};
-            } else {
-                return {};
-            }
-        }
-
-        /// The typeid for the held object
+        /// #### The typeid for the held object
         auto const &held_type() const {
             if (not type) {
-                throw std::runtime_error{"There is no held object"};
+                throw felspar::stdexcept::logic_error{
+                        "There is no held object"};
             } else {
                 return *type;
             }
         }
 
+
+        /// ### Modification
+
+        /// #### Return a reference to the contained object
+        template<typename T>
+        T &value() {
+            if (typeid(T) == *type) {
+                return unsafe_value<T>();
+            } else {
+                throw felspar::stdexcept::logic_error{
+                        "This any_buffer is empty"};
+            }
+        }
+        template<typename T>
+        T const &value() const {
+            if (typeid(T) == *type) {
+                return unsafe_value<T>();
+            } else {
+                throw felspar::stdexcept::logic_error{
+                        "This any_buffer is empty"};
+            }
+        }
+
+
       private:
+        void destroy() {
+            if (deleter) { deleter(buffer.data()); }
+        }
+        template<typename T>
+        T &unsafe_value() {
+            return *reinterpret_cast<T *>(buffer.data());
+        }
+        template<typename T>
+        T const &unsafe_value() const {
+            return *reinterpret_cast<T *>(buffer.data());
+        }
+
+        std::array<std::byte, buffer_size> buffer alignas(alignment);
         std::type_info const *type = nullptr;
         void (*move_into)(std::byte *, std::byte *) = nullptr;
         void (*deleter)(std::byte *) = nullptr;
-        std::array<std::byte, buffer_size> buffer alignas(alignment);
     };
 
 
