930f9ca032420df09dc931c6a0af1db49e5e14ba
Kirit Sælensminde <kirit@felspar.com>
2024-05-19 13:08:17 +0700

    clang-format

--- test/run/hexdump.cpp
+++ test/run/hexdump.cpp
@@ -19,28 +19,32 @@ namespace {
                 std::stringstream ss;
                 std::array<std::byte, 16> a{std::byte{'a'}};
                 felspar::memory::hexdump(ss, a);
-                check(ss.str()) == "Showing 16 bytes out of 16\n"
-                    "| 61 00 00 00 00 00 00 00 | 00 00 00 00 00 00 00 00 |   |a°°°°°°°|°°°°°°°°|\n";
+                check(ss.str()) ==
+                        "Showing 16 bytes out of 16\n"
+                        "| 61 00 00 00 00 00 00 00 | 00 00 00 00 00 00 00 00 |   |a°°°°°°°|°°°°°°°°|\n";
             },
             [](auto check) {
                 std::stringstream ss;
                 std::array<std::byte, 16> a{std::byte{'a'}};
                 ss << felspar::memory::hexdump(a);
-                check(ss.str()) == "Showing 16 bytes out of 16\n"
-                    "| 61 00 00 00 00 00 00 00 | 00 00 00 00 00 00 00 00 |   |a°°°°°°°|°°°°°°°°|\n";
+                check(ss.str()) ==
+                        "Showing 16 bytes out of 16\n"
+                        "| 61 00 00 00 00 00 00 00 | 00 00 00 00 00 00 00 00 |   |a°°°°°°°|°°°°°°°°|\n";
             },
             [](auto check) {
                 std::stringstream ss;
                 std::array<char, 16> a{'a', 'b', 'c'};
                 ss << felspar::memory::hexdump(a);
-                check(ss.str()) == "Showing 16 bytes out of 16\n"
-                    "| 61 62 63 00 00 00 00 00 | 00 00 00 00 00 00 00 00 |   |abc°°°°°|°°°°°°°°|\n";
+                check(ss.str()) ==
+                        "Showing 16 bytes out of 16\n"
+                        "| 61 62 63 00 00 00 00 00 | 00 00 00 00 00 00 00 00 |   |abc°°°°°|°°°°°°°°|\n";
             },
             [](auto check) {
                 std::array<char, 16> a{'a', 'b', 'c'};
                 std::string hd = felspar::memory::hexdump(a, 8);
-                check(hd) == "Showing 8 bytes out of 16\n"
-                    "| 61 62 63 00 00 00 00 00 |                         |   |abc°°°°°|        |\n";
+                check(hd) ==
+                        "Showing 8 bytes out of 16\n"
+                        "| 61 62 63 00 00 00 00 00 |                         |   |abc°°°°°|        |\n";
             });
 
 
